b50ddb396dddf9855e1803c0c743dc5bd6bdda6a
Catalin <4375373-cat@users.noreply.gitlab.com>
2025-01-20 21:07:47 +0200

    chore(tmux): make catppuccin status work, add ressurrect/continuum

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -17,14 +17,30 @@
         plugin = tmuxPlugins.catppuccin;
         extraConfig = '' 
           set -g @catppuccin_flavour 'mocha'
-          set -g @catppuccin_window_tabs_enabled on
-          set -g @catppuccin_date_time "%H:%M"
-          set -g @catppuccin_window_status_style "rounded"
+          set -g @catppuccin_date_time_text "%H:%M"
+          set -g @catppuccin_status_justify "centre"
+          set -g @catppuccin_status_modules_right "date_time application uptime cpu battery"
         '';
       }
       tmuxPlugins.battery
       tmuxPlugins.cpu
       # tmuxPlugins.better-mouse-mode
+      {
+          plugin = tmuxPlugins.resurrect;
+          extraConfig = ''
+          set -g @resurrect-strategy-vim 'session'
+          set -g @resurrect-strategy-nvim 'session'
+          set -g @resurrect-capture-pane-contents 'on'
+          '';
+      }
+      {
+          plugin = tmuxPlugins.continuum;
+          extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-boot 'on'
+          set -g @continuum-save-interval '10'
+          '';
+      }
     ];
     extraConfig = builtins.readFile ./tmux.conf;
   };
