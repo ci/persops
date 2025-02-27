6471f5b51850f6356b393f24046b2170552c6c1a
Catalin <4375373-cat@users.noreply.gitlab.com>
2025-02-27 18:22:00 +0200

    chore: disable tmux ressurect/continuum

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -27,23 +27,24 @@
       }
       tmuxPlugins.battery
       tmuxPlugins.cpu
+      # disable for the time being, not acting nicely with tmuxinator
       # tmuxPlugins.better-mouse-mode
-      {
-          plugin = tmuxPlugins.resurrect;
-          extraConfig = ''
-          set -g @resurrect-strategy-vim 'session'
-          set -g @resurrect-strategy-nvim 'session'
-          set -g @resurrect-capture-pane-contents 'on'
-          '';
-      }
-      {
-          plugin = tmuxPlugins.continuum;
-          extraConfig = ''
-          set -g @continuum-restore 'on'
-          set -g @continuum-boot 'on'
-          set -g @continuum-save-interval '10'
-          '';
-      }
+      # {
+      #     plugin = tmuxPlugins.resurrect;
+      #     extraConfig = ''
+      #     set -g @resurrect-strategy-vim 'session'
+      #     set -g @resurrect-strategy-nvim 'session'
+      #     set -g @resurrect-capture-pane-contents 'on'
+      #     '';
+      # }
+      # {
+      #     plugin = tmuxPlugins.continuum;
+      #     extraConfig = ''
+      #     set -g @continuum-restore 'on'
+      #     set -g @continuum-boot 'on'
+      #     set -g @continuum-save-interval '10'
+      #     '';
+      # }
     ];
     extraConfig = builtins.readFile ./tmux.conf;
   };
