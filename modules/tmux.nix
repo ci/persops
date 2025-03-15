f23485024f5ed0bf5f4520b2a5e48ac3feb1598f
Catalin <4375373-cat@users.noreply.gitlab.com>
2025-03-15 20:31:29 +0200

    fix(tmux): catppuccin upgrade config changes

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -18,11 +18,22 @@
         extraConfig = ''
           set -g @catppuccin_flavour 'mocha'
           set -g @catppuccin_date_time_text "%H:%M"
-          set -g @catppuccin_status_justify "centre"
           set -g @catppuccin_status_modules_right "date_time application uptime cpu battery"
           set -g @catppuccin_window_default_text "#W"
           set -g @catppuccin_window_text "#W"
           set -g @catppuccin_window_current_text "#W"
+
+          set -g @catppuccin_window_status_style "rounded"
+
+          set -g status-justify "centre"
+          set -g status-right-length 100
+          set -g status-left-length 100
+          set -g status-left ""
+          set -ag status-right "#{E:@catppuccin_status_date_time}"
+          set -g status-right "#{E:@catppuccin_status_application}"
+          set -ag status-right "#{E:@catppuccin_status_uptime}"
+          set -agF status-right "#{E:@catppuccin_status_cpu}"
+          set -agF status-right "#{E:@catppuccin_status_battery}"
         '';
       }
       tmuxPlugins.battery
