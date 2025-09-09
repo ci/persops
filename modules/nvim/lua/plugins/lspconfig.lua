f2fd3af1e77b1eef2a85710ec87e7ac325ad47e4
Catalin <4375373-cat@users.noreply.gitlab.com>
2025-09-10 02:12:21 +0300

    chore: slightly increase tsserver mem, fix some monorepo

---
+++ modules/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,20 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      vtsls = {
+        settings = {
+          typescript = {
+            tsserver = {
+              maxTsServerMemory = 4096,
+              pluginPaths = {
+                "./node_modules",
+                "./chat/node_modules", -- monorepo problems
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
