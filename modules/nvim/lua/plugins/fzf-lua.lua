761bb08c0805f9722f5350829d317ba7a0da6546
Catalin <4375373-cat@users.noreply.gitlab.com>
2025-09-17 21:12:33 +0300

    chore(nvim): drop not-working fzf-lua custom action keybind

--- modules/nvim/lua/plugins/fzf-lua.lua
+++ modules/nvim/lua/plugins/fzf-lua.lua
@@ -1,25 +1,14 @@
 return {
   "ibhagwan/fzf-lua",
-  opts = function(_, _)
-    local fzf = require("fzf-lua")
-    local actions = fzf.actions
-
-    return {
-      grep = { rg_glob = true },
-      oldfiles = {
-        include_current_session = true,
+  opts = {
+    grep = { rg_glob = true },
+    oldfiles = {
+      include_current_session = true,
+    },
+    previewers = {
+      builtin = {
+        syntax_limit_b = 1024 * 100, -- 100KB
       },
-      previewers = {
-        builtin = {
-          syntax_limit_b = 1024 * 100, -- 100KB
-        },
-      },
-      actions = {
-        files = {
-          ["alt-i"] = { actions.toggle_ignore },
-          ["alt-e"] = { actions.toggle_hidden },
-        },
-      },
-    }
-  end,
+    },
+  },
 }
