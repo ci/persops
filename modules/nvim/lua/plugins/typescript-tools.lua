830a3146fcd8af55905e744154983ff19329997a
Catalin <4375373-cat@users.noreply.gitlab.com>
2025-11-05 23:48:44 +0200

    feat(nvim): better ts errors

--- modules/nvim/lua/plugins/typescript-tools.lua
+++ modules/nvim/lua/plugins/typescript-tools.lua
@@ -1,9 +1,38 @@
 return {
   {
+    "davidosomething/format-ts-errors.nvim",
+  },
+  {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
     opts = {
+      handlers = {
+        ["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
+          if result.diagnostics == nil then
+            return
+          end
+
+          -- ignore some tsserver diagnostics
+          local idx = 1
+          while idx <= #result.diagnostics do
+            local entry = result.diagnostics[idx]
+
+            local formatter = require("format-ts-errors")[entry.code]
+            entry.message = formatter and formatter(entry.message) or entry.message
+
+            -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+            if entry.code == 80001 then
+              -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+              table.remove(result.diagnostics, idx)
+            else
+              idx = idx + 1
+            end
+          end
+
+          vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+        end,
+      },
       settings = {
         -- tsserver_path = "~/.bun/bin/tsgo",
         separate_diagnostic_server = true,
