d6cd56daaf7cc35f56e7289159ace2f232509578
Catalin <4375373-cat@users.noreply.gitlab.com>
2025-12-05 23:19:47 +0200

    fix(nvim): handle prettier with biome

--- modules/nvim/lua/config/options.lua
+++ modules/nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.lazyvim_prettier_needs_config = false
+vim.g.lazyvim_prettier_needs_config = true
 vim.g.tmux_navigator_disable_when_zoomed = 1
 
 vim.g.ai_cmp = false -- use inline suggestions instead of cmp
