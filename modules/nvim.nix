8681b02fb7e630aa45c64cd6552174eababa890a
Catalin <4375373-cat@users.noreply.gitlab.com>
2025-12-12 00:47:49 +0200

    chore(nvim): useful comment for quick local config testing

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ config, ... }: {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
@@ -8,6 +8,7 @@
 
   xdg.configFile."nvim" = {
     source = ./nvim;
+    # source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/p/persops/modules/nvim";
     recursive = true;
   };
 }
