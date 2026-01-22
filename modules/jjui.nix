3ad08033ed323f538c2502c5dedc89f512ff730f
Catalin <4375373-cat@users.noreply.gitlab.com>
2026-01-22 17:18:39 +0200

    fix(jjui): use base16 catppuccin mocha

--- modules/jjui.nix
+++ modules/jjui.nix
@@ -2,8 +2,8 @@
 {
   xdg.configFile."jjui/config.toml".source = ./jjui.toml;
 
-  xdg.configFile."jjui/themes/base24-catppuccin-mocha.toml".source = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/vic/tinted-jjui/refs/heads/main/themes/base24-catppuccin-mocha.toml";
-    hash = "sha256-MhUxsNsNlGpkLIfWCD/rV35O1J0HorzH9uLFLkILMGE=";
+  xdg.configFile."jjui/themes/base16-catppuccin-mocha.toml".source = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/vic/tinted-jjui/refs/heads/main/themes/base16-catppuccin-mocha.toml";
+    hash = "sha256-bB9KCfjd0QzcO9aIRFW1lD27MVKzNHHNI1b74jL7N1o=";
   };
 }
