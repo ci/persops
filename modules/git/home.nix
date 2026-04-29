3a8e4b36c70c15a3a5afd74c46e756f5857e6f72
Catalin <4375373-cat@users.noreply.gitlab.com>
2026-04-29 21:23:42 +0300

    chore(git): trust ssh commit signatures locally

--- modules/git/home.nix
+++ modules/git/home.nix
@@ -1,13 +1,40 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }:
+
+let
+  workSshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGzV767GrAPq9JZ/Iv7B4Yg6wiA2AH2AwjnZWE23r3HX";
+  personalSshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFP05x9Bg50efrFPX0NXfV45RwcsYmgpKUKTnR2Ee7LA";
+in
+{
   home.file = {
     # also can use a ~/.gitconfig.local with non-committed overrides
     ".gitconfig".source = ./gitconfig;
   };
 
-  # Take ownership of Git's default global excludes file.
-  xdg.configFile."git/ignore" = {
-    source = ./ignore;
-    force = true;
+  xdg.configFile = {
+    # Take ownership of Git's default global excludes file.
+    "git/ignore" = {
+      source = ./ignore;
+      force = true;
+    };
+
+    # Local trust DB for SSH commit/tag signatures.
+    # Work key: GitHub cat-ph SSH signing key 858365.
+    # Personal key: GitHub ci SSH auth key; ci currently publishes no SSH signing keys.
+    "git/allowed_signers" = {
+      text = ''
+        catalin.i@posthog.com,268578347+cat-ph@users.noreply.github.com,cat-ph@users.noreply.github.com namespaces="git" ${workSshKey}
+        catalin.irimie@gmail.com,6650666+ci@users.noreply.github.com,ci@users.noreply.github.com namespaces="git" ${personalSshKey}
+      '';
+      force = true;
+    };
+
+    "git/1password-ssh-signing.inc" = {
+      text = lib.optionalString pkgs.stdenv.isDarwin ''
+        [gpg "ssh"]
+            program = /Applications/1Password.app/Contents/MacOS/op-ssh-sign
+      '';
+      force = true;
+    };
   };
 
   home.packages = with pkgs; [
