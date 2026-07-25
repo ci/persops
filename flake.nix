08aa5d155c027b33df16b1fb9927adbc8533e8cf
Catalin <4375373-cat@users.noreply.gitlab.com>
2026-07-25 03:43:57 +0300

    chore(herdr): update to 0.7.5

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     herdr = {
-      url = "github:ogulcancelik/herdr/v0.7.3";
+      url = "github:ogulcancelik/herdr/v0.7.5";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     jj-starship = {
